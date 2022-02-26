2b2c5cdf5cd6006b83cc2da6c023a87fb386c80e
Khang Phung <97179275+KhangPhungNN@users.noreply.github.com>
2022-02-26 14:12:58 +0700

    Delete Definition directory

--- Definition/Convert_text_to_QR_code.py
+++
@@ -1,8 +0,0 @@
-#Convert text to QR code (v.1)
-import qrcode
-from PIL import Image
-data = input("Enter the data to be converted: ")
-name = input("Enter the name of the file: ")
-img = qrcode.make(data)
-img.save(name+".jpg")
-f = Image.open(name+".jpg").show()
